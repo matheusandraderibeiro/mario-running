2d2fc9f78caf1818f8bdf4f245eb6a4cdec6e416
Matheus de Andrade Ribeiro <matheusdeandraderibeiro@gmail.com>
2026-08-04 11:01:32 -0300

    Refatoração

--- src/js/volume.js
+++ src/js/volume.js
@@ -51,7 +51,7 @@ export function controleVolume() {
         controleVolume.addEventListener("input", ()=> {
             atualizarBarra(controleVolume);
             atualizarIcon(botao, icon, controleVolume.value);
-            
+
             if(audioAlvo) {
                 audioAlvo.volume = controleVolume.value / 100;
             }
@@ -61,101 +61,16 @@ export function controleVolume() {
         });
     });
 
-    window.addEventListener("load", () => {
-        if(!audio) return;
-        audio.play().catch(()=> {
-            const iniciar = ()=> {
-                audio.play();
-                document.removeEventListener("click", iniciar);
-                document.removeEventListener("keydown", iniciar);
-            }
-            document.addEventListener("click", iniciar);
-            document.addEventListener("keydown", iniciar);
-        });
-    });
+    // window.addEventListener("load", () => {
+    //     if(!audio) return;
+    //     audio.play().catch(()=> {
+    //         const iniciar = ()=> {
+    //             audio.play();
+    //             document.removeEventListener("click", iniciar);
+    //             document.removeEventListener("keydown", iniciar);
+    //         }
+    //         document.addEventListener("click", iniciar);
+    //         document.addEventListener("keydown", iniciar);
+    //     });
+    // });
 }
-
-
-
-
-
-
-// export function controleVolume() {
-//     const audio = document.getElementById("telaInicialTheme");
-//     const efeito = document.getElementById("volumeEfeitos");
-//     const botoes = document.querySelectorAll(".optBotao");
-//     const som = document.getElementById("volumeMusica");
-//     let volumes = [som, efeito];
-
-//     function controleVolume(input) {
-//         const val = Number(input.value);
-//         const min = Number(input.min) || 0;
-//         const max = Number(input.max) || 100;
-//         const porcentagem = ((val - min) / (max - min)) * 100;
-
-//         input.style.background = `linear-gradient(to right, #ff0000 0%, #ff0000 ${porcentagem}%, #ddd ${porcentagem}%, #ddd 100%)`;
-//     }
-
-//     function verificaIcon(icon) {
-//         if(icon.classList.contains("iconVolume")) {
-//             icon.classList.toggle("iconSemVolume");
-//         } else {
-//             icon.classList.toggle("iconSemEfeito");
-//         }
-//     }
-
-//     botoes.forEach((botao) => {
-//         const conteiner = botao.closest("div");
-//         const volume = conteiner.querySelector(".volume");
-//         const audioAlvo = document.getElementById(volume.dataset.audio);
-
-//         botao.addEventListener("click", (event) => {
-//             const botaoClicado = event.currentTarget;
-//             const icon = botao.querySelector("span");
-            
-//             if(Number(volume.value) > 0) {
-//                 volume.dataset.volume = volume.value;
-//                 volume.value = 0;
-//                 verificaIcon(icon);
-//             } else {
-//                 volume.value = volume.dataset.volume || 50;
-//                 verificaIcon(icon);
-//             }
-            
-//             controleVolume(volume);
-//             if(audioAlvo) {
-//                 audioAlvo.volume = volume.value / 100;
-//             }
-//         });
-//     });
-    
-//     volumes.forEach((volume) => {
-//         const audioAlvo = document.getElementById(volume.dataset.audio);
-//         controleVolume(volume);
-        
-//         volume.addEventListener("input", () => {
-//             controleVolume(volume);
-//             const icon = document.querySelector(".iconVolume");
-
-//             if(audioAlvo) {
-//                 audioAlvo.volume = volume.value / 100;
-//             }
-//             if(audioAlvo.id === "efeitosSonoros") {
-//                 audioAlvo.play();
-//             }
-//         });
-//     });
-
-//     window.addEventListener('load', () => {
-//         if(!audio) return;
-//         audio.play().catch(() => {
-//             const iniciar = () => {
-//                 audio.play();
-//                 document.removeEventListener('click', iniciar);
-//                 document.removeEventListener('keydown', iniciar);
-//             };
-//             document.addEventListener('click', iniciar);
-//             document.addEventListener('keydown', iniciar);
-//         });
-//     });
-// }
